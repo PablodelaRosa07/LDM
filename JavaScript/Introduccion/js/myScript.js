c1206c67cb1ff87959473f89139387de7c0562ef
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-12-16 13:32:36 +0100

    subo

---
+++ JavaScript/Introduccion/js/myScript.js
@@ -0,0 +1,14 @@
+//alert ("Hello World")
+var table = "Normal Table";
+let chair = "One Chair";
+console.log(table)
+console.log(chair)
+
+let name = 'Pablo';
+let surname = 'de la Rosa';
+let age = 23;
+let question = `How are you ${name} ${surname}?`;
+let answer = `He is ${age} years old`;
+console.log(question);
+console.log(answer);
+
